da0829246fc4879363be788edc9fd5cc0e24a821
YunyangSir <602714426@qq.com>
2019-02-12 13:11:58 +0800

    city-search done

--- wheretogo/src/views/city/components/Search.vue
+++ wheretogo/src/views/city/components/Search.vue
@@ -1,12 +1,82 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="请输入城市名或拼音">
+    <div>
+        <div class="search">
+            <input v-model="keyword" class="search-input" type="text" placeholder="请输入城市名或拼音">
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item"
+                    v-for="item of list"
+                    :key="item.id"
+                    @click="handleCityClick(item.name)"
+                >
+                    {{item.name}}
+                </li>
+                <li class="search-item" v-show="hasNoData">
+                    没有找到匹配数据
+                </li>
+            </ul>
+        </div>
     </div>
+
 </template>
 
 <script>
+  import Bscroll from 'better-scroll';
+  import { mapMutations } from 'vuex'
   export default {
-    name: "Search"
+    name: "Search",
+    props: {
+      cities: Object
+    },
+    data () {
+      return {
+        keyword: '',
+        list: [],
+        timer: null
+      }
+    },
+    methods: {
+      handleCityClick (city) {
+        this.changeCity(city)
+        this.$router.push('/');
+      },
+      ...mapMutations(['changeCity'])
+    },
+    computed: {
+      hasNoData () {
+        return !this.list.length
+      }
+    },
+    watch: {
+      keyword () {
+        if (this.timer) {
+          clearTimeout(this.timer);
+        }
+        if (!this.keyword) {
+          this.list = []
+          return
+        }
+        this.timer = setTimeout(() => {
+          const result = [];
+          for (let i in this.cities) {
+            this.cities[i].forEach((value) => {
+              if (value.spell.indexOf(this.keyword) > -1 ||
+                value.name.indexOf(this.keyword) > -1) {
+                result.push(value)
+              }
+            })
+          }
+          this.list = result;
+        }, 100)
+      }
+    },
+    mounted () {
+      // 默认没有加‘click: true’，需要手动添加，不然无法触发click事件
+      this.scroll = new Bscroll(this.$refs.search, {
+        click: true
+      })
+    }
   }
 </script>
 
@@ -24,5 +94,19 @@
             line-height: .62rem
             text-align: center
             border-radius: .06rem
+            color: #666
+    .search-content
+        z-index: 1
+        overflow: hidden
+        position: absolute
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+        background: #eee
+        .search-item
+            line-height: .62rem
+            padding-left: .2rem
+            background: #fff
             color: #666
 </style>
--- wheretogo/src/views/home/components/Header.vue
+++ wheretogo/src/views/home/components/Header.vue
@@ -18,10 +18,13 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+  // import { mapGetters } from 'vuex'
   export default {
     name: "Header",
-    props: {
-      city: String
+    computed: {
+      ...mapState(['city']),
+      // ...mapGetters(['doubleCity'])
     }
   }
 </script>
@@ -50,7 +53,8 @@
             border-radius: .1rem
             color: #ccc
         .header-right
-            width: 1.24rem
+            min-width: 1.04rem
+            padding: 0 .1rem
             float: right
             text-align: center
             color: #fff
